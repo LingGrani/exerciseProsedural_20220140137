ca2dcfd46834fa13c5dd781286b57823ba8fdfe9
LingGrani <luthfiangger2510@gmail.com>
2023-03-27 10:25:28 +0700

    prosedur input

--- exerciseProsedural_20220140137/exerciseProsedural_20220140137.cpp
+++ exerciseProsedural_20220140137/exerciseProsedural_20220140137.cpp
@@ -6,3 +6,35 @@ int jumlahNama;
 double NilaiMTK[20];
 double NilaiBhsInggris[20];
 double Nilairata[20];
+void input_string()
+{  //prosedure input
+	while (true)
+	{
+		cout << "masukan jumlah data pada array : ";
+		cin >> jumlahNama;
+
+		if (jumlahNama <= 20)
+		{
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan maksimal 20 element. \n";
+		}
+	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukan nama nama" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < jumlahNama; i++)
+	{
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> kadidat[i];							
+	}
+}
+
+int main()
+{
+
+}
