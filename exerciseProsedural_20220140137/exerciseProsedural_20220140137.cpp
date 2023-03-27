955af7f20de3dbd92eec5874b21ce92e7ff8cab0
LingGrani <luthfiangger2510@gmail.com>
2023-03-27 10:25:51 +0700

    Input nilai

--- exerciseProsedural_20220140137/exerciseProsedural_20220140137.cpp
+++ exerciseProsedural_20220140137/exerciseProsedural_20220140137.cpp
@@ -34,6 +34,20 @@ void input_string()
 	}
 }
 
+void input_double(string mapel, double nilai[])
+{  //prosedure input
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukan nilai " << mapel << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < jumlahNama; i++)
+	{
+		cout << "nilai " << mapel << " " << kadidat[i] << (i + 1) << ": ";
+		cin >> nilai[i];
+	}
+}
+
 int main()
 {
 
